9cbeecb8ec215eb4e51ff2d0bf521a1cad5299db
Ol' Farmer McNinja <olfarmermcninjaeve@gmail.com>
2021-07-01 18:41:34 -0700

    framework
    Added basic framework for button game commands

--- cogs/ButtonGame.py
+++ cogs/ButtonGame.py
@@ -11,6 +11,16 @@ class ButtonGame(commands.Cog):
     the top five longest timestamps and the player who pressed it.
     - Players can see their rank among everyone on the servervia the rank
     subcommand.
+
+    serverID.button datastructure:
+    {
+        'users':[
+                {userID: SilentCinema, score: 1893, timestamp: 1622649960},
+                {userID: Panyk, score: 2453, timestamp: 1622649990},
+                {userID: Omn1core, score 5435, timestamp: 1622650017}
+                ]
+        'LastTimestamp': 1622650039
+    }
     """
 
     def __init__(self, bot):
@@ -25,6 +35,12 @@ class ButtonGame(commands.Cog):
         #open .dat file and save to PlayerData variable
         #last_timestamp = last timestamp from .dat file
 
+    #LeaderboardEmbed()
+
+    #rankEmbed()
+
+    #buttonPressEmbed()
+
     @commands.group(case_insensitive=True)
     async def button(self, ctx):
         if ctx.invoked_subcommand is None:
@@ -34,14 +50,27 @@ class ButtonGame(commands.Cog):
     @button.command()
     async def Press(self, ctx):
         #CurrentTimestamp = timestamp
+        #open serverID.button as ButtonData
+        #scoreList = ButtonData['users']
+        #index = next((i for i, item in enumerate(scoreList) if item["userID"] == ctx.UserID), None)
+        #scoreList[index]['timestamp']=CurrentTimestamp
+        #scoreList[index]['score'] = CurrentTimestamp - ButtonData['lastTimestamp']
+        #newlist = sorted(scoreList, key=itemgetter('score'), reverse=True)
+        #ButtonData['lastTimestamp'] = CurrentTimestamp
+        #ButtonData['users'] = newlist
+        #pickle ButtonData to serverID.button
+        #close serverID.button
         await ctx.send("Button Press")
 
     @button.command()
     async def Rank(self, ctx):
+        #index = next((i for i, item in enumerate(scoreList) if item["userID"] == ctx.UserID), None)
+        #rank = index+1
         await ctx.send("Rank")
 
     @button.command()
     async def List(self, ctx):
+        #ButtonData['users'][range(0,5)]
         await ctx.send("Leaderboard")
 
 def setup(bot):
